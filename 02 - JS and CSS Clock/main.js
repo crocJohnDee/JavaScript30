916442ad7428e748ef386d1e6289a3204ab6cee8
crocJohnDee <johnn.barendrecht@hotmail.com>
2019-08-18 16:28:35 +0200

    changed turnery to if - else

--- 02 - JS and CSS Clock/main.js
+++ 02 - JS and CSS Clock/main.js
@@ -6,20 +6,29 @@ function setDate() {
 
     const seconds = now.getSeconds();
     const secondDegrees = ((seconds / 60) * 360) + 90;
-    secondDegrees === 90 ? secondHand.style.transition = "none" :
+    if (secondDegrees === 90) {
+        secondHand.style.transition = "none";
+    } else {
         secondHand.style.transition = "all 0.05s cubic-bezier(0.25, 2.34, 0.58, 1) 0s";
+    }
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
 
     const minutes = now.getMinutes();
     const minuteDegrees = ((minutes / 60) * 360) + 90;
-    minuteDegrees === 90 ? minHand.style.transition = "none" :
+    if (minuteDegrees === 90) {
+        minHand.style.transition = "none";
+    } else {
         minHand.style.transition = "all 0.05s cubic-bezier(0.25, 2.34, 0.58, 1) 0s";
+    }
     minHand.style.transform = `rotate(${minuteDegrees}deg)`;
 
     const hours = now.getHours();
     const hourDegrees = ((hours / 12) * 360) + 90;
-    hourDegrees === 90 ? hourHand.style.transition = "none" :
+    if (hourDegrees === 90) {
+        hourHand.style.transition = "none";
+    } else {
         hourHand.style.transition = "all 0.05s cubic-bezier(0.25, 2.34, 0.58, 1) 0s";
+    }
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
